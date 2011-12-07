f830ec4de7da486b993b75972d0a1a71e338e460
git_name <git_email>
2011-12-07 10:03:09 -0700

    Pushing new changes

---
+++ source/prep.specs/CodeKata/BloomFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prep.specs.CodeKata
+{
+    class BloomFilter
+    {
+        const int filterSize = 10;
+        
+        BitArray filter = new BitArray(filterSize);
+
+        public void AddElement(string element)
+        {
+            int hashCode = element.GetHashCode();
+
+            int index = hashCode%filterSize;
+
+            filter[index] = true;
+        }
+
+
+        public bool Contains(string element)
+        {
+            int hashCode = element.GetHashCode();
+
+            int index = hashCode % filterSize;
+
+            return filter[index];
+
+        }
+
+
+
+    }
+}
