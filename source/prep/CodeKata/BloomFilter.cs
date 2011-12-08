2114bc0b018f88dd6d09c64484872218df5f698d
git_name <git_email>
2011-12-08 09:58:31 -0700

    Pushing new changes

---
+++ source/prep/CodeKata/BloomFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prep.CodeKata
+{
+    class BloomFilter
+    {
+        private const int filterSize = 10; 
+        BitArray filter = new BitArray(filterSize);
+
+        public void AddElement(string element)
+        {
+            foreach (int hash in Hasher<string>.GetHash(element))
+                filter[hash%filterSize] = true;
+        }
+
+        public bool Contains(string element)
+        {
+            foreach (int hash in Hasher<string>.GetHash(element))
+           
+            return filter[hash % filterSize];
+        }
+
+    }
+}
---
+++ source/prep/CodeKata/Hasher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prep.CodeKata
+{
+    class Hasher<T>
+    {
+        static int NumberOfHashFunction { get; set; }
+        
+        //public  static int GetHash(T element)
+        //{
+        //    return element.GetHashCode();
+        //}
+
+        public static IEnumerable<int> GetHash(T element)
+        {
+            for (int i = 0; i < NumberOfHashFunction; i++)
+            {
+               yield return (element.GetHashCode() & i) << 16;
+            }
+        }
+
+
+    }
+}
